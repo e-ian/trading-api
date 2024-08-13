5fc29676a25d7ce2dc7ce5c1234c8e9337f4c1e3
e-ian <ianemma70@gmail.com>
2024-08-13 20:16:20 +0300

    - add secret key

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    app.secret_key = os.getenv('SECRET_KEY')
+    app.secret_key = os.getenv('SECRET_KEY', 'eebd61a1c94d7fe7696f584cd523418e06d4a247a1ff29fb')
 
     app.register_blueprint(api_blueprint, url_prefix='/forex_tickers')
 
